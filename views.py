5b9eb00b496d9632f328f2083fd5d634b8292de4
SbrtNpl <nepals@carolinau.edu>
2024-10-15 17:42:34 -0400

    Add files via upload

--- views.py
+++ views.py
@@ -1,15 +1,3 @@
-from django.http import JsonResponse
-
-def hello_world(request):
-    return JsonResponse({"message": "Hello, world from Django!"})
-from django.shortcuts import render
-
-# Create your views here.
-from django.http import HttpResponse
-
-def home(request):
-    return HttpResponse("Welcome to Help!")
-
 from rest_framework import viewsets
 from .models import ForumPost
 from .serializers import ForumPostSerializer
